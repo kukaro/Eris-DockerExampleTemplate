a8ceb364a1a2746981eeaa1340823dc3f86ab887
kukaro <justkukaro@naver.com>
2018-05-26 17:43:31 +0900

    v1.3.1 add vue

---
+++ Apache-FrontEnd.Bower-Flask-Python-SPA.vue-Ubuntu/FLASKAPPS/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/hi')
+def hi():
+    return 'hi'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ Apache-FrontEnd.Bower-Flask-Python-SPA.vue-Ubuntu/FLASKAPPS/helloworldapp.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0,"/var/www/")
+from FLASKAPPS import app as application
