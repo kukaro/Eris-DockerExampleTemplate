c615b16864a3fb97f3170eabc7f8a4ba94cefa46
kukaro <justkukaro@naver.com>
2019-12-17 20:55:53 +0900

    asdf

--- Z-TEST/KAFKA-TEST/Ubuntu-Sshd-Kafka_SpringBootCloud/myubuntu/KafkaProject/src/main/java/com/example/demo/DemoApplication.java
+++ Z-TEST/KAFKA-TEST/Ubuntu-Sshd-Kafka_SpringBootCloud/myubuntu/KafkaProject/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @SpringBootApplication
+@EnableBinding(Processor.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@StreamListener(Processor.INPUT)
+	@SendTo(Processor.OUTPUT)
+	public String handle(String value) {
+		System.out.println("Received: " + value);
+		return value.toUpperCase();
+	}
 }
