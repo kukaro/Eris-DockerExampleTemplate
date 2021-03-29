8a85b62ee76e3619ecac132d5c60cada5b660405
kukaro <justkukaro@naver.com>
2021-03-30 02:24:45 +0900

    feat: add vue-router

--- Nginx-SpringBoot-Vuetify/project/my-app/src/main.js
+++ Nginx-SpringBoot-Vuetify/project/my-app/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
 
 new Vue({
   vuetify,
