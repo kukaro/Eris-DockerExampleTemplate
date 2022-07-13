69f466b0b80a30ac1e761fa96080506375ed8976
Coeat.k <justkukaro@naver.com>
2022-07-13 16:06:02 +0900

    feat: 네트워크 설정으로 완전 자동화 성공

--- Z-READY/Ubuntu-Sshd-Zookeeper/myzookeeper/test/init.sh
+++ Z-READY/Ubuntu-Sshd-Zookeeper/myzookeeper/test/init.sh
@@ -6,4 +6,5 @@ a=$(expr match "$a" '\(inet[0-9]*.[0-9]*.[0-9]*.[0-9]*\)')
 echo "${a:4}" > /root/myip/ip.txt
 
 cp /root/myip/myid /var/lib/zookeeper/myid
+/usr/share/zookeeper/bin/zkServer.sh start
 /usr/sbin/sshd -D
